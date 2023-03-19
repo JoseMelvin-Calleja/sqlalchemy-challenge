45cf11ea410b260ffe687301fec3bb1bfead0691
JoseMelvin-Calleja <jcalleja@bates.edu>
2023-03-19 15:22:22 -0400

    fixed flask app

--- ClimateAnalysis/app.py
+++ ClimateAnalysis/app.py
@@ -4,6 +4,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import datetime as dt
+import numpy as np
+
 
 # importing Flask 
 from flask import Flask, jsonify
@@ -33,25 +35,32 @@ def homepage():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
     )
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
+    session = Session(engine)
+
     max_date = session.query(func.max(measurement.date))
     for dates in max_date:
         recent_date = dt.datetime.strptime(dates[0], '%Y-%m-%d')
     one_year_ago = recent_date - dt.timedelta(days = 365)
-
-    session = Session(engine)
 
     results = session.query(measurement.date, measurement.prcp).\
                     filter(measurement.date >= one_year_ago).all()
     
     session.close()
 
-    return jsonify(results)
+    percip = []
+    for date, precipitation in results:
+        percip_dict = {}
+        percip_dict['Date'] = date
+        percip_dict['Precipitation'] = precipitation
+        percip.append(percip_dict)
+
+    return jsonify(percip)
 
 @app.route('/api/v1.0/stations')
 def stations():
@@ -59,30 +68,39 @@ def stations():
     session = Session(engine)
 
     # retrieving station names
-    station_names = session.query(station.station)
+    results = session.query(station.station).all()
 
     session.close()
 
-    return jsonify(station_names)
+    station_list = list(np.ravel(results))
+
+    return jsonify(station_list)
 
 @app.route('/api/v1.0/tobs')
 def tobs():
+    session = Session(engine)
+
     # calculating date from 12 months ago
     max_date = session.query(func.max(measurement.date))
     for dates in max_date:
         recent_date = dt.datetime.strptime(dates[0], '%Y-%m-%d')
     one_year_ago = recent_date - dt.timedelta(days = 365)
 
-    session = Session(engine)
-
     # retrieving past 12 months temperature data from the most active station
     results = session.query(measurement.date, measurement.tobs).\
-                filter((measurement.station == 'USC00519281').\
-                filter(measurement.date >= one_year_ago))
+                filter(measurement.station == 'USC00519281').\
+                filter(measurement.date >= one_year_ago)
     
     session.close()
 
-    return jsonify(results)
+    data = []
+    for date, tob in results:
+        dict = {}
+        dict['Date'] = date
+        dict['TOBS'] = tob
+        data.append(dict)
+
+    return jsonify(data)
 
 @app.route('/api/v1.0/<start>')
 def start_analysis(start):
@@ -93,7 +111,14 @@ def start_analysis(start):
     
     session.close()
 
-    return jsonify(results)
+    data = []
+    for date, tob in results:
+        dict = {}
+        dict['Date'] = date
+        dict['TOBS'] = tob
+        date.append(dict)
+
+    return jsonify(data)
 
 @app.route('/api/v1.0/<start>/<end>')
 def range_analysis(start, end):
@@ -104,7 +129,14 @@ def range_analysis(start, end):
     
     session.close()
 
-    return jsonify(results)
+    data = []
+    for date, tob in results:
+        dict = {}
+        dict['Date'] = date
+        dict['TOBS'] = tob
+        date.append(dict)
+
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
